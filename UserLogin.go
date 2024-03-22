c1d972e9ce215270260d7e5cde76790539dfc41e
2333hunyuan <zydyouxiang@outlook.com>
2024-03-22 23:02:38 +0800

    fix bug

--- UserLogin.go
+++ UserLogin.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
 )
 
@@ -18,15 +20,26 @@ func Userlogin(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	stmt := "SELECT * FROM User WHERE username =? AND password = ?"
-	row := database.QueryRow(stmt, json.Username, json.Password)
-	var DB Login
-	err := row.Scan(&DB.Username, &DB.Password)
-	switch {
-	case err == sql.ErrNoRows:
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Interstitials"})
-	case err != nil:
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching user from the database"})
+	prepstmt := "SELECT COUNT(*) FROM User WHERE Username =? AND Password = ?"
+	stmt, preperr := database.Prepare(prepstmt)
+	if preperr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error preparing statement"})
+		return
 	}
-	c.JSON(http.StatusOK, gin.H{"status": "200"})
+	var count int64
+	// 使用 _ 作为占位符，因为我们只关心查询是否成功
+	err := stmt.QueryRow(json.Username, json.Password).Scan(&count)
+	switch {
+	case errors.Is(err, sql.ErrNoRows):
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+	case err != nil:
+		log.Printf("Unexpected error while fetching user: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching user from the database"})
+	default:
+		if count > 0 {
+			c.JSON(http.StatusOK, gin.H{"status": "200"})
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		}
+	}
 }
