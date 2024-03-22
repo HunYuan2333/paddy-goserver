0d30d0cba855a299df3100a0c9287413e6a22df1
2333hunyuan <zydyouxiang@outlook.com>
2024-03-22 21:39:57 +0800

    SQL ADD

--- Main.go
+++ Main.go
@@ -1,15 +1,22 @@
 package paddygoserver
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
-type Login struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
+var database *sqlx.DB
 
+func init() {
+	database, err := sqlx.Open("mysql", "root:13376035511@tcp(127.0.0.1:3306)/Paddy")
+	if err != nil {
+		fmt.Println("open mysql failed,", err)
+		return
+	}
+}
 func Main() {
 	r := gin.Default()
-	r.POST("userlogin", userlogin)
+	r.POST("userlogin", Userlogin)
 }
--- UserLogin.go
+++ UserLogin.go
@@ -1,16 +1,35 @@
 package paddygoserver
 
 import (
+	"database/sql"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
 
-func userlogin(c *gin.Context) {
+type Login struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func Userlogin(c *gin.Context) {
 	var json Login
 	if err := c.ShouldBindJSON(&json); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	//TODO:SQl operate
+	stmt := "SELECT * FROM User WHERE username =? AND password = ?"
+	row := database.QueryRow(stmt, json.Username, json.Password)
+	var DB Login
+	err := row.Scan(&DB.Username, &DB.Password)
+	switch {
+	case err == sql.ErrNoRows:
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Interstitials"})
+	case err != nil:
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching user from the database"})
+	default:
+		c.JSON(http.StatusOK, gin.H{"status": "200"})
+
+	}
 	c.JSON(http.StatusOK, gin.H{"status": "200"})
 }
